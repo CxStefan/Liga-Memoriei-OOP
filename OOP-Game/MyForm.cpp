bf64be129a6c31090fd0bc194c5224d744dc6a52
Cumpanasu Stefan - Adrian <cumpanasu.stefan.a5z@student.ucv.ro>
2025-01-05 16:23:13 +0200

    MyForm.cpp

--- OOP-Game/MyForm.cpp
+++ OOP-Game/MyForm.cpp
@@ -9,24 +9,7 @@ int main(array<System::String^>^ args)
     Application::SetCompatibleTextRenderingDefault(false);
     OOPGame::MyForm form;
     Application::Run(% form);
-
- //   Leaderboard^ leaderboard = gcnew Leaderboard();
- //   Player^ player = gcnew Player("John", 100);
- //   Player^ player2 = gcnew Player("Jane", 200);
- //   Player^ player3 = gcnew Player("Stefan", 930);
- //   Player^ player4 = gcnew Player("Andrei", 650);
- //   Player^ player5 = gcnew Player("UserGuest", 780);
-
- //   leaderboard->addPlayer(player);
- //   leaderboard->addPlayer(player2);
- //   leaderboard->addPlayer(player3); 
- //   leaderboard->addPlayer(player4);
- //   leaderboard->addPlayer(player5);
-
- //   List<Player^>^ players = leaderboard->readPlayers(); 
- //   leaderboard->sortPlayers(players); 
-	//leaderboard->writePlayers(players);
-
+	
     return 0;
 }
 
